e7b9c34dda668769f5049780fef26f4eb9c424c6
Ayman <Ayman@LENOVO>
2017-08-30 02:31:32 +0000

    Add project files.

---
+++ Chacking Cart/Form2.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Chacking_Cart
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtreg_no = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtdep = new System.Windows.Forms.TextBox();
+            this.txtlevel = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lab_time = new System.Windows.Forms.Label();
+            this.labname = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REG NO :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Department :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Level :";
+            // 
+            // txtreg_no
+            // 
+            this.txtreg_no.Location = new System.Drawing.Point(181, 64);
+            this.txtreg_no.Name = "txtreg_no";
+            this.txtreg_no.ReadOnly = true;
+            this.txtreg_no.Size = new System.Drawing.Size(272, 27);
+            this.txtreg_no.TabIndex = 4;
+            this.txtreg_no.Text = "0001";
+            this.txtreg_no.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(181, 107);
+            this.txtname.Name = "txtname";
+            this.txtname.ReadOnly = true;
+            this.txtname.Size = new System.Drawing.Size(272, 27);
+            this.txtname.TabIndex = 5;
+            this.txtname.Text = "Ayman nabil ali hezam";
+            this.txtname.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtdep
+            // 
+            this.txtdep.Location = new System.Drawing.Point(181, 150);
+            this.txtdep.Name = "txtdep";
+            this.txtdep.ReadOnly = true;
+            this.txtdep.Size = new System.Drawing.Size(272, 27);
+            this.txtdep.TabIndex = 6;
+            this.txtdep.Text = "programmer";
+            this.txtdep.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtlevel
+            // 
+            this.txtlevel.Location = new System.Drawing.Point(181, 193);
+            this.txtlevel.Name = "txtlevel";
+            this.txtlevel.ReadOnly = true;
+            this.txtlevel.Size = new System.Drawing.Size(272, 27);
+            this.txtlevel.TabIndex = 7;
+            this.txtlevel.Text = "4";
+            this.txtlevel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(516, 59);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.AliceBlue;
+            this.groupBox1.Controls.Add(this.lab_time);
+            this.groupBox1.Controls.Add(this.labname);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 269);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(712, 33);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // lab_time
+            // 
+            this.lab_time.AutoSize = true;
+            this.lab_time.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lab_time.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lab_time.Location = new System.Drawing.Point(563, 16);
+            this.lab_time.Name = "lab_time";
+            this.lab_time.Size = new System.Drawing.Size(0, 14);
+            this.lab_time.TabIndex = 2;
+            // 
+            // labname
+            // 
+            this.labname.AutoSize = true;
+            this.labname.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labname.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labname.Location = new System.Drawing.Point(76, 13);
+            this.labname.Name = "labname";
+            this.labname.Size = new System.Drawing.Size(74, 14);
+            this.labname.TabIndex = 1;
+            this.labname.Text = "Ayman nabil";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(14, 13);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 14);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Welcome :";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(710, 301);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.txtlevel);
+            this.Controls.Add(this.txtdep);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtreg_no);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form2";
+            this.Text = "Checking Card";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtreg_no;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtdep;
+        private System.Windows.Forms.TextBox txtlevel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lab_time;
+        private System.Windows.Forms.Label labname;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
